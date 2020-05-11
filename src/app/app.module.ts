3ac2a715a77533441ea8e482ec05a691225b23e7
oshek <oshek14@freeuni.edu.ge>
2020-05-11 16:30:43 +0400

    eu-label

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EuButtonComponent } from './shared-components/eu-button/eu-button.component';
+import { EuLabelComponent } from './shared-components/eu-label/eu-label.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EuButtonComponent,
+    EuLabelComponent,
   ],
   imports: [
     BrowserModule
